d83ce4b091b74e6bd6a7fff868037ea88042c394
Aleksandra-Borodaneva <aleksandraborodaneva1@gmail.com>
2022-11-22 10:21:12 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -38,3 +38,15 @@ function setEnglish() {
 
     document.getElementById('reset-button').value= 'reset';
 }
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userName);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+
