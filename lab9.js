3f9127455caefde3da41a14360b2557b49878dad
Aleksandra-Borodaneva <aleksandraborodaneva1@gmail.com>
2022-12-20 10:35:49 +0700

    Переключение состояний

--- lab9.js
+++ lab9.js
@@ -10,3 +10,12 @@ function clickName(button){
      else
          button.innerHTML = 'Александра';
  }
+ function clickOn(button){
+    if(button.innerHTML == 'Нажми меня') 
+         button.innerHTML = 'Еще раз нажми';
+    else if(button.innerHTML == 'Еще раз нажми') 
+         button.innerHTML = 'Еще';
+    else if(button.innerHTML == 'Еще') 
+         button.innerHTML = 'Начать сначала';
+    else button.innerHTML = 'Нажми меня';
+ }
