a3c52ece6ee996197c9446df2f9788607ac47d7d
Aleksandra-Borodaneva <aleksandraborodaneva1@gmail.com>
2022-12-20 10:24:52 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -1,6 +1,12 @@
 function clickMe(button){
-   if(button.innerHTML = 'Щелчок') 
+   if(button.innerHTML == 'Щелчок') 
         button.innerHTML = 'Спасибо';
     else
         button.innerHTML = 'Щелчок';
 }
+function clickName(button){
+    if(button.innerHTML == 'Александра') 
+         button.innerHTML = 'Бороданева';
+     else
+         button.innerHTML = 'Александра';
+ }
